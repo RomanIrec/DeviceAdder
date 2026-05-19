46d9f628b75bac990b07e682fd4a338600331441
Roman Novytskyi <rnovytskyi@irec.cat>
2026-05-19 15:36:04 +0200

    yes

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,6 +25,11 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3001",
+        "http://172.16.36.113:3001",
     ],
     allow_credentials=True,
     allow_methods=["*"],
